c3a7710b27d3f4d4e0e486718c9fe7c1b8419b6c
KMZ0209 <sedmikrasky0209@gmail.com>
2023-11-09 00:49:01 +0900

    不要なファイル削除,アロー関数式,テンプレートリテラル,err部分を修正しました

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,40 +1,36 @@
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(":memory");
+const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE IF NOT EXISTS books_table (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
     db.run(
-      "INSERT INTO books_table (title) VALUES (?)",
+      "INSERT INTO books (title) VALUES (?)",
       ["Cherry Book 1"],
       function () {
-        const lastID1 = this.lastID;
-        console.log("lastID:" + lastID1);
+        console.log(`lastID: ${this.lastID}`);
         db.run(
-          "INSERT INTO books_table (title) VALUES (?)",
+          "INSERT INTO books (title) VALUES (?)",
           ["Cherry Book 2"],
           function () {
-            const lastID2 = this.lastID;
-            console.log("lastID:" + lastID2);
+            console.log(`lastID: ${this.lastID}`);
             db.run(
-              "INSERT INTO books_table (title) VALUES (?)",
+              "INSERT INTO books (title) VALUES (?)",
               ["Cherry Book 3"],
               function () {
-                const lastID3 = this.lastID;
-                console.log("lastID:" + lastID3);
-                db.each(
-                  "SELECT id, title FROM books_table",
-                  function (err, row) {
-                    console.log(row.id, row.title);
-                  },
-                );
-                db.run("DROP TABLE IF EXISTS books_table", function () {});
-              },
+                console.log(`lastID: ${this.lastID}`);
+                db.each("SELECT * FROM books", (_, row) => {
+                  console.log(row.id, row.title);
+                });
+              }
             );
-          },
+          }
         );
-      },
+      }
     );
-  },
+  }
 );
+db.run("DROP TABLE books", () => {
+  db.close();
+});
--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -1,66 +1,40 @@
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(":memory");
+const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE IF NOT EXISTS books_table (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
-    db.run(
-      "INSERT INTO books_table (title) VALUES (null)",
-      function (insertErr) {
-        if (insertErr) {
-          console.error("エラー1 inserting record: " + insertErr.message);
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run("INSERT INTO books (title) VALUES (null)", (err) => {
+      if (err) {
+        console.error(`エラー1 inserting record: ${err.message}`);
+      } else {
+        console.log(`Inserted row with ID: ${this.lastID}`);
+      }
+      db.run("INSERT INTO books (title) VALUES (null)", (err) => {
+        if (err) {
+          console.error(`エラー2 inserting record: ${err.message}`);
         } else {
-          console.log("Inserted row with ID: " + this.lastID);
+          console.log(`Inserted row with ID: ${this.lastID}`);
         }
-        db.run(
-          "INSERT INTO books_table (title) VALUES (null)",
-          function (insertErr) {
-            if (insertErr) {
-              console.error("エラー2 inserting record: " + insertErr.message);
+        db.run("INSERT INTO books (title) VALUES (null)", (err) => {
+          if (err) {
+            console.error(`エラー3 inserting record: ${err.message}`);
+          } else {
+            console.log(`Inserted row with ID: ${this.lastID}`);
+          }
+          db.each("SELECT * FROM nonexistent", (err, row) => {
+            if (err) {
+              console.error(`エラー selecting record: ${err.message}`);
             } else {
-              console.log("Inserted row with ID: " + this.lastID);
+              console.log(row.id, row.title);
             }
-            db.run(
-              "INSERT INTO books_table (title) VALUES (null)",
-              function (insertErr) {
-                if (insertErr) {
-                  console.error(
-                    "エラー3 inserting record: " + insertErr.message,
-                  );
-                } else {
-                  console.log("Inserted row with ID: " + this.lastID);
-                }
-                db.each(
-                  "SELECT id, title FROM books_table",
-                  function (selectErr, row) {
-                    if (selectErr) {
-                      console.error(
-                        "エラー2 selecting record: " + selectErr.message,
-                      );
-                    } else {
-                      console.log(row.id, row.title);
-                    }
-                  },
-                  (completeErr) => {
-                    if (completeErr) {
-                      console.error(
-                        "エラー3 completing select operation: " +
-                          completeErr.message,
-                      );
-                    } else {
-                      db.run(
-                        "DROP TABLE IF EXISTS books_table",
-                        function () {},
-                      );
-                    }
-                  },
-                );
-              },
-            );
-          },
-        );
-      },
-    );
-  },
+          });
+        });
+      });
+    });
+  }
 );
+db.run("DROP TABLE books", () => {
+  db.close();
+});
