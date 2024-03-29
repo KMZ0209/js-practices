928df91cd37ee41f6a7e3fe03c6fc88b8f6edb33
moegi29 <sedmikrasky0209@gmail.com>
2024-03-29 23:11:54 +0900

    不要な空行とif文、変数名をfor文を修正しました

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -5,36 +5,27 @@ import {
   closePromise,
 } from "./sqlite_promise_functions.js";
 
-async function main() {
-  const db = new sqlite3.Database(":memory:");
+const db = new sqlite3.Database(":memory:");
 
-  await runPromise(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
-  );
-
-  const result1 = await runPromise(db, "INSERT INTO books (title) VALUES (?)", [
-    "CherryBook1",
-  ]);
-  console.log(`lastID: ${result1.lastID}`);
-
-  const result2 = await runPromise(db, "INSERT INTO books (title) VALUES (?)", [
-    "CherryBook2",
-  ]);
-  console.log(`lastID: ${result2.lastID}`);
-
-  const result3 = await runPromise(db, "INSERT INTO books (title) VALUES (?)", [
-    "CherryBook3",
-  ]);
-  console.log(`lastID: ${result3.lastID}`);
-
-  const rows = await allPromise(db, "SELECT * FROM books");
-  for (const row of rows) {
-    console.log(row.id, row.title);
-  }
-
-  await runPromise(db, "DROP TABLE books");
-  await closePromise(db);
-}
-
-main();
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+);
+let result = await runPromise(db, "INSERT INTO books (title) VALUES (?)", [
+  "CherryBook1",
+]);
+console.log(`lastID: ${result.lastID}`);
+result = await runPromise(db, "INSERT INTO books (title) VALUES (?)", [
+  "CherryBook2",
+]);
+console.log(`lastID: ${result.lastID}`);
+result = await runPromise(db, "INSERT INTO books (title) VALUES (?)", [
+  "CherryBook3",
+]);
+console.log(`lastID: ${result.lastID}`);
+const rows = await allPromise(db, "SELECT * FROM books");
+rows.forEach((row) => {
+  console.log(row.id, row.title);
+});
+await runPromise(db, "DROP TABLE books");
+await closePromise(db);
--- 03.asynchronous/async_await_error.js
+++ 03.asynchronous/async_await_error.js
@@ -5,71 +5,46 @@ import {
   closePromise,
 } from "./sqlite_promise_functions.js";
 
-async function main() {
-  const db = new sqlite3.Database(":memory:");
+const db = new sqlite3.Database(":memory:");
 
-  await runPromise(
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+);
+try {
+  const result = await runPromise(
     db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+    "INSERT INTO books (title) VALUES (NULL)"
   );
-
-  try {
-    const result1 = await runPromise(
-      db,
-      "INSERT INTO books (title) VALUES (NULL)"
-    );
-    console.log(`lastID: ${result1.lastID}`);
-  } catch (err) {
-    if (err) {
-      console.error(`エラー1 inserting record: ${err.message}`);
-    } else {
-      throw err;
-    }
-  }
-
-  try {
-    const result2 = await runPromise(
-      db,
-      "INSERT INTO books (title) VALUES (NULL)"
-    );
-    console.log(`lastID: ${result2.lastID}`);
-  } catch (err) {
-    if (err) {
-      console.error(`エラー2 inserting record: ${err.message}`);
-    } else {
-      throw err;
-    }
-  }
-
-  try {
-    const result3 = await runPromise(
-      db,
-      "INSERT INTO books (title) VALUES (NULL)"
-    );
-    console.log(`lastID: ${result3.lastID}`);
-  } catch (err) {
-    if (err) {
-      console.error(`エラー3 inserting record: ${err.message}`);
-    } else {
-      throw err;
-    }
-  }
-
-  try {
-    const rows = await allPromise(db, "SELECT * FROM nonexistent");
-    for (const row of rows) {
-      console.log(row.id, row.title);
-    }
-  } catch (err) {
-    if (err) {
-      console.error(`エラー selecting record: ${err.message}`);
-    } else {
-      throw err;
-    }
-  }
-
-  await runPromise(db, "DROP TABLE books");
-  await closePromise(db);
+  console.log(`lastID: ${result.lastID}`);
+} catch (err) {
+  console.error(`エラー1 inserting record: ${err.message}`);
 }
-
-main();
+try {
+  const result = await runPromise(
+    db,
+    "INSERT INTO books (title) VALUES (NULL)"
+  );
+  console.log(`lastID: ${result.lastID}`);
+} catch (err) {
+  console.error(`エラー2 inserting record: ${err.message}`);
+}
+try {
+  const result = await runPromise(
+    db,
+    "INSERT INTO books (title) VALUES (NULL)"
+  );
+  console.log(`lastID: ${result.lastID}`);
+} catch (err) {
+  console.error(`エラー3 inserting record: ${err.message}`);
+}
+try {
+  const rows = await allPromise(db, "SELECT * FROM nonexistent");
+  rows.forEach((row) => {
+    console.log(row.id, row.title);
+  });
+} catch (err) {
+  console.error(`エラー selecting record: ${err.message}`);
+}
+await runPromise(db, "DROP TABLE books");
+await closePromise(db);
