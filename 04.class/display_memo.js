0885c7262ddb6abc0a3501bd5923576fa4572c58
moegi29 <sedmikrasky0209@gmail.com>
2024-09-08 18:39:34 +0900

    loadAndHandleMemoData関数を作成

--- 04.class/display_memo.js
+++ 04.class/display_memo.js
@@ -18,27 +18,39 @@ export default class MemoDisplayer {
       console.error("データ取得に失敗しました", error);
     }
   }
-  async saveMemoData(data) {
+  async saveMemoData() {
     try {
-      await fs.writeFile(this.fileName, JSON.stringify(data));
+      await fs.writeFile(this.fileName, JSON.stringify(this.memoData));
     } catch (error) {
       console.error("データ保存に失敗しました", error);
     }
   }
-  async selectMemoData(){
+  async selectMemoData() {
     const choices = this.memoData.map((memo, index) => ({
-    name: `${index + 1} ${memo[0].substring(0, 10)}`,
-    value: index,
+      name: `${index + 1} ${memo[0].substring(0, 10)}`,
+      value: index,
     }));
     const prompt = new Select({
-    name: "memo",
-    message: "MemoList",
-    choices: choices,
+      name: "memo",
+      message: "MemoList",
+      choices: choices,
     });
     return await prompt.run();
   }
+  async loadAndHandleMemoData(callback) {
+    try {
+      await this.loadMemoData();
+      if (this.memoData.length > 0) {
+        await callback();
+      } else {
+        console.log("メモがありません。");
+      }
+    } catch (error) {
+      console.error("エラーが発生しました", error);
+    }
+  }
 
-  addMemo() {
+  async addMemo() {
     const reader = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
@@ -48,14 +60,11 @@ export default class MemoDisplayer {
     });
     reader.on("close", async () => {
       if (this.memos.length > 0) {
-        try {
-          await this.loadMemoData();
+        await this.loadAndHandleMemoData.call(this, async () => {
           this.memoData.push(this.memos);
           await this.saveMemoData(this.memoData);
           console.log("メモが追加されました。");
-        } catch (error) {
-          console.error("メモの追加に失敗しました:", error);
-        }
+        });
         this.memos = [];
       }
       reader.close();
@@ -63,52 +72,30 @@ export default class MemoDisplayer {
   }
 
   async displayMemoList() {
-    try {
-      await this.loadMemoData();
-      console.log("入力されたメモリスト");
-      if (this.memoData.length > 0) {
-        this.memoData.forEach((memo, index) => {
-          if (memo.length > 0) {
-            console.log(`${index + 1} ${memo[0].substring(0, 10)}`);
-          }
-        });
-      } else {
-        console.log("メモがありません。");
-      }
-    } catch (error) {
-      console.error("メモリストの表示に失敗しました:", error);
-    }
+    await this.loadAndHandleMemoData.call(this, () => {
+      this.memoData.forEach((memo, index) => {
+        if (memo.length > 0) {
+          console.log(`${index + 1} ${memo[0].substring(0, 10)}`);
+        }
+      });
+    });
   }
 
   async displayFullMemo() {
-    try {
-      await this.loadMemoData();
-      if (this.memoData.length > 0) {
-        const selectIndex = await this.selectMemoData();
-        const memoNumber = parseInt(selectIndex[0].trim(), 10) - 1;
-        console.log(this.memoData[memoNumber].join("\n"));
-      } else {
-        console.log("メモがありません。");
-      }
-    } catch (error) {
-      console.error("エラーが発生しました:", error);
-    }
+    await this.loadAndHandleMemoData.call(this, async () => {
+      const selectIndex = await this.selectMemoData();
+      const memoNumber = parseInt(selectIndex[0].trim(), 10) - 1;
+      console.log(this.memoData[memoNumber].join("\n"));
+    });
   }
 
   async deleteMemo() {
-    try {
-      await this.loadMemoData();
-      if (this.memoData.length > 0) {
-        const selectIndex = await this.selectMemoData();
-        const memoNumber = parseInt(selectIndex[0].trim(), 10) - 1;
-        this.memoData.splice(memoNumber, 1);
-        await this.saveMemoData(this.memoData);
-        console.log("選択したメモを削除しました");
-      } else {
-        console.log("削除するメモがありません。");
-      }
-    } catch (error) {
-      console.error("エラーが発生しました:", error);
-    }
+    await this.loadAndHandleMemoData.call(this, async () => {
+      const selectValue = await this.selectMemoData();
+      const memoNumber = parseInt(selectValue[0].trim(), 10) - 1;
+      this.memoData.splice(memoNumber, 1);
+      await this.saveMemoData(this.memoData);
+      console.log("選択したメモを削除しました");
+    });
   }
 }
